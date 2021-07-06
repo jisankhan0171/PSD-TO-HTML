595854f4df4ed666f8d8e814c99a9de8507bd100
jisan khan <abjisan17@gmail.com>
2021-07-06 19:45:19 +0600

    first commit

---
+++ resourcess/js/main.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function(){
+   $('svg.radial-progress').each(function( index, value ) { 
+  $(this).find($('circle.complete')).removeAttr( 'style' );
+}); 
+    var mixer = mixitup('.mymix');
+    
+});
